040a32f69d020104dd06a6c4421f4ce4b27e56a9
huanglijiao <huanglijiao1990@163.com>
2015-09-29 18:26:31 +0800

    修改 anchor.js

--- widget/anchor/anchor.js
+++ widget/anchor/anchor.js
@@ -2,8 +2,9 @@ define(function (require, exports, module) {
 	var $ = require('jquery');
     //锚点滚动
 	exports.anchor = function (option) {
-		var duringtime = option ? option.duringtime || 500 : "";
-		var ele = option ? option.ele : "";
+		if(!option) return;
+		var duringtime = option.duringtime || 500;
+		var ele = option.ele;
 		if(!ele) return;
 		var top = $(ele).offset().top;
 		$("html,body").animate({scrollTop: top}, duringtime);	
