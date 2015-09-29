f743e213c7fe43fb7fe5852f2916824726152df0
FrancesShih <shiyongxin_327@aliyun.com>
2015-09-29 22:14:25 +0800

    ci scroll

--- assets/script/home/init.js
+++ assets/script/home/init.js
@@ -4,15 +4,43 @@
 define(function (require, exports, module) {
     var $ = require('jquery');
     var widget = require('widget/slidebanner');
-    var insslide = new widget.slide({
-        container: '.slidebanner .banner',
-        slide: '.slidebanner .banner > li',
-        handler: '.slidebanner .dot-index'
-    });
+    
     var insslide2 = new widget.slide({
         container: '.showcase .showcase-banner',
         slide: '.showcase .showcase-banner > li',
         handler: '.showcase .showcase-tab',
         fits: true
+    });
+
+    // 滚动
+    var g = window;
+    var posArr = [1282, 2075, 2571, 2923];
+    var eArr = ['.round-inline', '.cloudy', '.cloudy-m'];
+    var e1 = $(eArr[0]),
+        e2 = $(eArr[1]),
+        e3 = $(eArr[2]),
+        eA = $(eArr.join(','));
+
+    $(g).on('scroll', function () {
+         var pos = g.scrollY;
+         
+         if(pos < posArr[0]) {
+            eA.removeClass('active');
+         }
+         if (pos >= posArr[0] && pos < posArr[1] && !e1.hasClass('active') ) {
+            eA.removeClass('active');
+            e1.addClass('active');
+         }
+         if (pos >= posArr[1] && pos < posArr[2] && !e2.hasClass('active') ) {
+            eA.removeClass('active');
+            e2.addClass('active');
+         }
+         if (pos >= posArr[2] && pos < posArr[3] && !e3.hasClass('active') ) {
+            eA.removeClass('active');
+            e3.addClass('active');
+         }
+         if(pos >= posArr[3]) {
+            eA.removeClass('active');
+         }
     });
 });
