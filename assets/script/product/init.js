2c79de481cf38e5b597dd54cf9f8d23a321042a3
FrancesShih <shiyongxin_327@aliyun.com>
2015-10-03 13:25:39 +0800

    ci production

--- assets/script/product/init.js
+++ assets/script/product/init.js
@@ -3,4 +3,19 @@
 */
 define(function (require, exports, module) {
     var $ = require('jquery');
+    var widget = require('widget/anchor');
+    var tab = $('.tab li'); 
+
+    tab.each(function (i) {
+        $(this).on('click', function () {
+            tab.removeClass('active');
+            $(this).addClass('active');
+            var _index = i >> 0;
+            _index = _index + 1;
+            widget.anchor({
+                ele: 'h2:nth-of-type('+ _index + ')' 
+            });
+        });
+    });
+
 });
