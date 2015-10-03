9cb527291042b5772bee24c6d5003ce4278414cf
FrancesShih <shiyongxin_327@aliyun.com>
2015-10-03 23:33:47 +0800

    ci 加入我们

--- widget/leftbar-a/leftbar-a.js
+++ widget/leftbar-a/leftbar-a.js
@@ -5,13 +5,38 @@ define(function (require, exports, module) {
     var $ = require('jquery');
     // menu
     $('.menu li').each(function (i){
+        // init
+        var hasChild = $(this).has('ol').size();
+        var heights = 0;
+        if (!!hasChild && !$(this).hasClass('active')) {
+            heights = $(this).find('ol').height();
+            $(this).find('ol').attr('data-h', heights).css({
+                'height': 0,
+                'position': 'static',
+                'visibility': 'visible'
+            });
+        }
+
+        // bind click
         $(this).on('click', function (e){
+            // 如果已经展开 神马也不做
+            if ($(this).hasClass('active')) {
+               if ($(this).has('ol').size() !==0) {
+                 e.preventDefault();
+               }
+               return;
+            }
+            // 收起其他的，这只展开就行了... 
             $('.menu li').removeClass('active');
-            $('.menu li > ol').hide();
-            $(this).addClass('actove');
-            if ($(this).has('ol')) {
+            $(this).addClass('active');
+            $('.menu li ol').animate({
+                height: 0
+            }, 300);
+            if ($(this).has('ol').size() !==0 ) {
                 e.preventDefault();
-                $(this).addClass('active').find('ol').show();
+                $(this).find('ol').animate({
+                    height: $(this).find('ol').attr('data-h') + 'px'
+                }, 300);
             }
         });
     });
