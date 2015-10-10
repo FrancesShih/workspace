07dcd296dffe7782cd89c203a3423a143fee73b6
huanglijiao <huanglijiao1990@163.com>
2015-10-10 17:07:51 +0800

    帮助中心左侧栏

--- widget/leftbar-help/leftbar-help.js
+++ widget/leftbar-help/leftbar-help.js
@@ -5,50 +5,46 @@ define(function (require, exports, module) {
     var $ = require('jquery');
     // menu
     $('.menu li').each(function (i){
-        // init
+        // init - replace has
         var $this = $(this);
-        $this.find('ol').each(function () {
-            $(this).children("li").each(function () {
-                if($(this).find("ol").length <= 0) {
-                    $(this).find('a').css({'background-image':'none'});
-                }
-            });
-        });
+        //给有子节点的添加图标
+        if($this.has('ol').length > 0) {
+            $(this).children('a').addClass('menu-bg');
+        } 
+        //通过子节点找到父节点
+        if($this.find('a').hasClass('selected')) {
+            $this.parents('ol').addClass('active');
+            $this.closest('li').children('a').addClass('menu-sub selected');
+        } 
+
         // bind click
         $this.on('click', function (e) {
             e.preventDefault();
             e.stopPropagation();
+            //! has ol return ;
             var p = $(this);
-            var $ol = $(this).children("ol");
-            if($ol.length > 0) {
-                if($ol.css('display') == 'none') {
-                    $ol.show(350);
-                    //变换收齐和展开的图标
-                    p.children('a').css('background-position-y', '-22px');
-                    if(p.parent().hasClass('menu')) return;
-                    //选中字体为红色
-                    p.parent().find('a').removeClass();
-                    p.parents('li').siblings().find('a').removeClass();
-                    p.children('a').attr('class', 'red');
-                } else {
-                    p.find("ol").hide(350);
-                    //变换收齐和展开的图标
-                    p.find('a').css('background-position-y', '5px');
-                    p.parent().find('a').removeClass();
-                }
-            } else {
-                //选中字体为红色
-                p.parent().find('a').removeClass();
-                p.parents('li').siblings().find('a').removeClass();
-                p.find('a').attr('class', 'red');
-                //变换收齐和展开的图标
-                if(p.find('a').css('background-position-y') == '-22px') {
-                    p.find('a').css('background-position-y', '5px');
-                } else {
-                    p.find('a').css('background-position-y', '-22px');
-                } 
-            }
+            var $ol = p.children("ol");
 
+            if(p.has('ol').length > 0) {
+                $ol.toggle(350, function () {
+                    /*
+                    if(p.children('a').hasClass('menu-sub')) {
+                        p.children('a').removeClass('menu-sub');
+                        p.children('a').addClass('menu-add');
+                    } else {
+                        p.children('a').removeClass('menu-add');
+                        p.children('a').addClass('menu-sub');
+                    }
+                    */
+                });
+                p.children('a').toggleClass(function () {
+                    if(p.children('a').hasClass('menu-sub')) {
+                        return 'menu-add';
+                    } else {
+                        return 'menu-sub';
+                    }
+                });
+            }
         });
     });
 });
