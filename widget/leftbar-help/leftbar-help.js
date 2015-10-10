37cca29e4918f27c5bc2d491136d0165e9dc2c77
huanglijiao <huanglijiao1990@163.com>
2015-10-10 12:49:27 +0800

    帮助中心子页面

--- widget/leftbar-help/leftbar-help.js
+++ widget/leftbar-help/leftbar-help.js
@@ -7,34 +7,46 @@ define(function (require, exports, module) {
     $('.menu li').each(function (i){
         // init
         var $this = $(this);
+        $this.find('ol').each(function () {
+            $(this).children("li").each(function () {
+                if($(this).find("ol").length <= 0) {
+                    $(this).find('a').css({'background-image':'none'});
+                }
+            });
+        });
         // bind click
         $this.on('click', function (e) {
             e.preventDefault();
             e.stopPropagation();
-
-            if($(this).children("ol").length > 0) {
-                if($(this).children("ol").css('display') == 'none') {
-                    $(this).children("ol").show(350);
-                    $(this).children('a').find('img').attr('src', '/assets/img/common/icon-helpdoc-sub.png');
-                    if($(this).parent().hasClass('menu')) return;
-                    $(this).parent().find('a').removeClass();
-                    $(this).parents('li').siblings().find('a').removeClass();
-                    $(this).children('a').attr('class', 'red');
+            var p = $(this);
+            var $ol = $(this).children("ol");
+            if($ol.length > 0) {
+                if($ol.css('display') == 'none') {
+                    $ol.show(350);
+                    //变换收齐和展开的图标
+                    p.children('a').css('background-position-y', '-22px');
+                    if(p.parent().hasClass('menu')) return;
+                    //选中字体为红色
+                    p.parent().find('a').removeClass();
+                    p.parents('li').siblings().find('a').removeClass();
+                    p.children('a').attr('class', 'red');
                 } else {
-                    $(this).find("ol").hide(350);
-                    $(this).find('img').attr('src', '/assets/img/common/icon-helpdoc-add.png');
-                    $(this).parent().find('a').removeClass();
-                } 
+                    p.find("ol").hide(350);
+                    //变换收齐和展开的图标
+                    p.find('a').css('background-position-y', '5px');
+                    p.parent().find('a').removeClass();
+                }
             } else {
-                $(this).parent().find('a').removeClass();
-                $(this).parents('li').siblings().find('a').removeClass();
-                $(this).find('a').attr('class', 'red');
-                if($(this).find('img').attr('src') == '/assets/img/common/icon-helpdoc-add.png') {
-                    $(this).find('img').attr('src', '/assets/img/common/icon-helpdoc-sub.png');
+                //选中字体为红色
+                p.parent().find('a').removeClass();
+                p.parents('li').siblings().find('a').removeClass();
+                p.find('a').attr('class', 'red');
+                //变换收齐和展开的图标
+                if(p.find('a').css('background-position-y') == '-22px') {
+                    p.find('a').css('background-position-y', '5px');
                 } else {
-                    $(this).find('img').attr('src', '/assets/img/common/icon-helpdoc-add.png');
+                    p.find('a').css('background-position-y', '-22px');
                 } 
-
             }
 
         });
